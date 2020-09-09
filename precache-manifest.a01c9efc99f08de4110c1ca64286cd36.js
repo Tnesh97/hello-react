d53dffa03075100c351c2b557624c54f23e3ddeb
THINESH <thinesh@LAPTOP-H04PMBVS>
2020-09-10 00:18:05 +0800

    Updates

---
+++ precache-manifest.a01c9efc99f08de4110c1ca64286cd36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f79db462d8c3adf7edcf46ad098cc4f",
+    "url": "/git@github.com:Tnesh97/hello-react.git/index.html"
+  },
+  {
+    "revision": "784054fa4ac769984706",
+    "url": "/git@github.com:Tnesh97/hello-react.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "efbbb9a2ff2b8deca739",
+    "url": "/git@github.com:Tnesh97/hello-react.git/static/js/2.421509d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/git@github.com:Tnesh97/hello-react.git/static/js/2.421509d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784054fa4ac769984706",
+    "url": "/git@github.com:Tnesh97/hello-react.git/static/js/main.595d912f.chunk.js"
+  },
+  {
+    "revision": "501fda5048fe0f8a91ab",
+    "url": "/git@github.com:Tnesh97/hello-react.git/static/js/runtime-main.2fdc265d.js"
+  },
+  {
+    "revision": "b0a09f0b2baf707cb7f4dc1ad990b22b",
+    "url": "/git@github.com:Tnesh97/hello-react.git/static/media/messi.b0a09f0b.jpg"
+  }
+]);
